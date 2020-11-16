ff56c180f05ec03aae8f9b6b53236c781faffe26
shyamal <shy@gmail.com>
2020-11-17 02:57:33 +0530

    first commit

---
+++ ppmt/src/main/java/com/barals/ppmt/ppmttool/PersonalProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.barals.ppmt.ppmttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
+	}
+
+}
---
+++ ppmt/src/test/java/com/barals/ppmt/ppmttool/PersonalProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barals.ppmt.ppmttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
